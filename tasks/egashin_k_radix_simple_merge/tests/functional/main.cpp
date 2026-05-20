3f517afeb4d757125daf71fe0ba29149b8a60eb0
crn4tt <kegashin@mail.ru>
2026-05-20 09:53:27 +0300

    omp done

--- tasks/egashin_k_radix_simple_merge/tests/functional/main.cpp
+++ tasks/egashin_k_radix_simple_merge/tests/functional/main.cpp
@@ -8,6 +8,7 @@
 #include <string>
 
 #include "egashin_k_radix_simple_merge/common/include/common.hpp"
+#include "egashin_k_radix_simple_merge/omp/include/ops_omp.hpp"
 #include "egashin_k_radix_simple_merge/seq/include/ops_seq.hpp"
 #include "util/include/func_test_util.hpp"
 #include "util/include/util.hpp"
@@ -59,8 +60,9 @@ const std::array<TestType, 8> kTestParam = {{
      "with_infinities"},
 }};
 
-const auto kTestTasksList =
-    ppc::util::AddFuncTask<EgashinKRadixSimpleMergeSEQ, InType>(kTestParam, PPC_SETTINGS_egashin_k_radix_simple_merge);
+const auto kTestTasksList = std::tuple_cat(
+    ppc::util::AddFuncTask<EgashinKRadixSimpleMergeSEQ, InType>(kTestParam, PPC_SETTINGS_egashin_k_radix_simple_merge),
+    ppc::util::AddFuncTask<EgashinKRadixSimpleMergeOMP, InType>(kTestParam, PPC_SETTINGS_egashin_k_radix_simple_merge));
 
 const auto kGtestValues = ppc::util::ExpandToValues(kTestTasksList);
 
