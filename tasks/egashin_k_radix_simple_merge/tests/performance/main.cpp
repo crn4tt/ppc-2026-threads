df12097b00ff988327dd7359350557724a25be7a
crn4tt <kegashin@mail.ru>
2026-05-20 09:53:27 +0300

    stl done

--- tasks/egashin_k_radix_simple_merge/tests/performance/main.cpp
+++ tasks/egashin_k_radix_simple_merge/tests/performance/main.cpp
@@ -7,6 +7,7 @@
 #include "egashin_k_radix_simple_merge/common/include/common.hpp"
 #include "egashin_k_radix_simple_merge/omp/include/ops_omp.hpp"
 #include "egashin_k_radix_simple_merge/seq/include/ops_seq.hpp"
+#include "egashin_k_radix_simple_merge/stl/include/ops_stl.hpp"
 #include "egashin_k_radix_simple_merge/tbb/include/ops_tbb.hpp"
 #include "util/include/perf_test_util.hpp"
 
@@ -46,7 +47,8 @@ namespace {
 
 const auto kAllPerfTasks =
     ppc::util::MakeAllPerfTasks<InType, EgashinKRadixSimpleMergeSEQ, EgashinKRadixSimpleMergeOMP,
-                                EgashinKRadixSimpleMergeTBB>(PPC_SETTINGS_egashin_k_radix_simple_merge);
+                                EgashinKRadixSimpleMergeSTL, EgashinKRadixSimpleMergeTBB>(
+        PPC_SETTINGS_egashin_k_radix_simple_merge);
 
 const auto kGtestValues = ppc::util::TupleToGTestValues(kAllPerfTasks);
 
